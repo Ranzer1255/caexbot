772c0373e69c739db4e06d6c64d358b4b106f0ce
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-09-22 13:20:33 -0500

    Shutdown Commands

---
+++ src/main/java/caexbot/commands/ShutdownCommand.java
@@ -0,0 +1,15 @@
+package caexbot.commands;
+
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+import net.dv8tion.jda.JDA;
+import net.dv8tion.jda.entities.Message;
+
+public class ShutdownCommand implements CommandExecutor {
+
+	@Command(aliases={"!sleep"}, description = "kill the bot", requiredPermissions = "player")
+	public void shutdown(){
+//		if (m.getAuthor().getUsername() == "Ranzer") 
+			System.exit(0);
+	}
+}
