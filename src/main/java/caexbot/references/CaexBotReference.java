2acbcea4c3d89db1a667ac3f9592389bcf9c4c89
Bobby <sgmaniac1255@yahoo.com>
2016-09-22 00:03:11 -0500

    add global command marker

--- src/main/java/caexbot/references/CaexBotReference.java
+++ src/main/java/caexbot/references/CaexBotReference.java
@@ -2,5 +2,7 @@ package caexbot.references;
 
 public class CaexBotReference {
 
-	public final static String TOKEN ="MjI4MjE5OTEzNTkwOTMxNDY4.CsSBFg.PyamWudzNqZTP7vmUUaCqifav_0"; 
+	public final static String TOKEN ="MjI4MjE5OTEzNTkwOTMxNDY4.CsSBFg.PyamWudzNqZTP7vmUUaCqifav_0";
+	public final static String COMMAND_LEAD = "Caex! ";
+	
 }
