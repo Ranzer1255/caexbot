2c4ecf9a1d6e2eb056daefbb9a0dd604cc220e2b
Bobby <sgmaniac1255@yahoo.com>
2016-09-24 22:59:29 -0500

    bla

--- src/main/java/caexbot/commands/EightBallCommand.java
+++ src/main/java/caexbot/commands/EightBallCommand.java
@@ -1,8 +1,15 @@
 package caexbot.commands;
 
+import java.util.List;
+
 import de.btobastian.sdcf4j.CommandExecutor;
 
 public class EightBallCommand implements CommandExecutor {
 	
+	private List<String> answers;
 	
+	public EightBallCommand(){
+		
+		
+	}
 }
