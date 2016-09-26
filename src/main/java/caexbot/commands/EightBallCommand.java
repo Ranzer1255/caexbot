855978bd361160ad232557e8988c0f464d60ae02
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-09-26 16:15:36 -0500

    load 8ball answers from file

--- src/main/java/caexbot/commands/EightBallCommand.java
+++ src/main/java/caexbot/commands/EightBallCommand.java
@@ -1,6 +1,10 @@
 package caexbot.commands;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import de.btobastian.sdcf4j.CommandExecutor;
 
@@ -10,6 +14,26 @@ public class EightBallCommand implements CommandExecutor {
 	
 	public EightBallCommand(){
 		
+		answers = loadAnswers();
+		
+		
+	}
+
+	private List<String> loadAnswers() {
+
+		List<String> rtn = new ArrayList<String>();
+		
+		try {
+			Scanner sc = new Scanner(new File("/caexbot/src/main/resources/8BallAnswers.txt"));
+			while (sc.hasNextLine()) {
+				rtn.add(sc.nextLine());				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return rtn;
 		
 	}
 }
