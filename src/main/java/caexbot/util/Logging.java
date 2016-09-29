eb30f44c80e4fbc62950e66867e54e26c0e2c063
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-09-29 10:05:56 -0500

    add calling class name to log

--- src/main/java/caexbot/util/Logging.java
+++ src/main/java/caexbot/util/Logging.java
@@ -12,9 +12,27 @@ public class Logging {
     private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss Z");
 
     public static void log(LogLevel level, String message) {
+//      CaexConfiguration config = CaexConfiguration.getInstance();
+
+      String line = String.format("[Unknown][%s\t%s] %s\n",level.name(), getTimestamp(), message);
+
+      System.out.print(line);
+
+//      try (BufferedWriter writer = new BufferedWriter(new FileWriter(config.getLogLocation(), true))) {
+//          if (level == LogLevel.DEBUG && !config.isDebug()) {
+//              return;
+//          }
+//
+//          writer.write(line);
+//      } catch (Exception ex) {
+//          System.out.println("Cannot log to file.");
+//          ex.printStackTrace();
+//      }
+  }
+    public static void log(Object caller, LogLevel level, String message) {
 //        CaexConfiguration config = CaexConfiguration.getInstance();
 
-        String line = String.format("[%s\t%s] %s\n", level.name(), getTimestamp(), message);
+        String line = String.format("[%s][%s\t%s] %s\n", caller.getClass().getCanonicalName(),level.name(), getTimestamp(), message);
 
         System.out.print(line);
 
@@ -30,16 +48,16 @@ public class Logging {
 //        }
     }
 
-    public static void debug(String message) {
-        log(LogLevel.DEBUG, message);
+    public static void debug(Object caller, String message) {
+        log(caller, LogLevel.DEBUG, message);
     }
 
-    public static void info(String message) {
-        log(LogLevel.INFO, message);
+    public static void info(Object caller, String message) {
+        log(caller, LogLevel.INFO, message);
     }
 
-    public static void error(String message) {
-        log(LogLevel.ERROR, message);
+    public static void error(Object caller, String message) {
+        log(caller, LogLevel.ERROR, message);
     }
 
     public static void log(Exception ex) {
