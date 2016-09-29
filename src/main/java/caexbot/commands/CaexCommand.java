36ba95745d54057ae6f77a4530d9665d772e667c
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-09-29 10:06:21 -0500

    add static prefex

--- src/main/java/caexbot/commands/CaexCommand.java
+++ src/main/java/caexbot/commands/CaexCommand.java
@@ -6,7 +6,15 @@ import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 public abstract class CaexCommand{
 
+	public static String CMD_PREFEX = "!";
+	
+	public static void setPrefex(String prefex){
+		CMD_PREFEX = prefex;
+	}
+
 	public void runCommand(String[] args, User author, TextChannel channel, MessageReceivedEvent event){} //TODO
 	
+	abstract public void process(String[] args, User author, TextChannel channel, MessageReceivedEvent event);
 	
+	abstract public String getUsage();
 }
