bc45e091eb7086732d6f449c7d3e82c17e62d89b
Bobby <sgmaniac1255@yahoo.com>
2016-09-29 23:07:59 -0500

    ...

--- src/main/java/caexbot/backgroundFunctions/CommandListener.java
+++ src/main/java/caexbot/backgroundFunctions/CommandListener.java
@@ -3,6 +3,7 @@ package caexbot.backgroundFunctions;
 import java.util.List;
 
 import caexbot.commands.CaexCommand;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 
 public class CommandListener extends ListenerAdapter {
@@ -12,6 +13,11 @@ public class CommandListener extends ListenerAdapter {
 	public void addCommand(CaexCommand cmd){
 		
 		this.cmds.add(cmd);
+	}
+	
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		
 	}
 
 }
