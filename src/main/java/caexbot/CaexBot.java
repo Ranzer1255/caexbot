d3f06245954ef9d49cc1ed6f3d6751e7c247229f
Bobby <sgmaniac1255@yahoo.com>
2016-10-01 12:49:55 -0500

    Bot Startup code

--- src/main/java/caexbot/CaexBot.java
+++ src/main/java/caexbot/CaexBot.java
@@ -1,7 +1,17 @@
 package caexbot;
 
+import java.nio.channels.ShutdownChannelGroupException;
+
+import caexbot.backgroundFunctions.CommandListener;
+import caexbot.backgroundFunctions.LevelUpdater;
+import caexbot.commands.EightBallCommand;
+import caexbot.commands.HelpCommand;
+import caexbot.commands.InfoCommand;
+import caexbot.commands.PingCommand;
+import caexbot.commands.ShutdownCommand;
 import caexbot.util.Logging;
 import net.dv8tion.jda.JDA;
+import net.dv8tion.jda.JDABuilder;
 
 public class CaexBot {
 	
@@ -10,7 +20,16 @@ public class CaexBot {
 	public static void main (String[] args){
 	Logging.info(null, "Huu... Wha... who... Oh, I guess it's time to [start up]");	
 	
+	CommandListener commands = new CommandListener();
 	
+	commands.addCommand(new HelpCommand(commands))
+			.addCommand(new InfoCommand())
+			.addCommand(new PingCommand())
+			.addCommand(new EightBallCommand())
+			.addCommand(new ShutdownCommand());
+	
+	JDABuilder build = new JDABuilder().addListener(commands)
+									   .addListener(new LevelUpdater());
 	}
 	
 	public static JDA getJDA(){
