902792bac0d16fc312d95b04a2fccca4471a8269
Bobby <sgmaniac1255@yahoo.com>
2016-09-30 20:47:15 -0500

    build listener for command calls

--- src/main/java/caexbot/CaexBot.java
+++ src/main/java/caexbot/CaexBot.java
@@ -1,8 +1,16 @@
 package caexbot;
 
+import net.dv8tion.jda.JDA;
+
 public class CaexBot {
+	
+	private static JDA JDA;
 
 	public static void main (String[] args){
 		
+	}
+	
+	public static JDA getJDA(){
+		return JDA;
 	}
 }
