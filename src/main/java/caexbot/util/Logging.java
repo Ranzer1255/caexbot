2320f0618cc285e18d1466dd4e183bc1bfa61f00
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-10-06 13:16:00 -0500

    fix logging
    closes #17

--- src/main/java/caexbot/util/Logging.java
+++ src/main/java/caexbot/util/Logging.java
@@ -1,7 +1,9 @@
 package caexbot.util;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -9,38 +11,47 @@ import caexbot.config.CaexConfiguration;
 
 public class Logging {
 
-    private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss Z");
+	private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss Z");
 
-    public static void log(LogLevel level, String message) {
-//        CaexConfiguration config = CaexConfiguration.getInstance();
+	public static void log(LogLevel level, String message) {
+		CaexConfiguration config = CaexConfiguration.getInstance();
 
-        String line = String.format("[%s\t%s] %s\n",level.name(), getTimestamp(), message);
+		String line = String.format("[%s\t%s] %s\n",level.name(), getTimestamp(), message);
 
-        System.out.print(line);
+		System.out.print(line);
 
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(config.getLogLocation(), true))) {
-//            if (level == LogLevel.DEBUG && !config.isDebug()) {
-//                return;
-//            }
-//
-//            writer.write(line);
-//        } catch (Exception ex) {
-//            System.out.println("Cannot log to file.");
-//            ex.printStackTrace();
-//        }
-    }
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(config.getLogLocation(), true))) {
+			if (level == LogLevel.DEBUG && !config.isDebug()) {
+				return;
+			}
 
-    public static void debug(String message) {
-        log(LogLevel.DEBUG, message);
-    }
+			writer.write(line);
+			writer.close();
+		}catch (FileNotFoundException e){
+			System.out.println("you shouldn't be going this way");
+			config.getLogLocation().getParentFile().mkdirs();
+			try {
+				config.getLogLocation().createNewFile();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}catch (Exception ex) {
+			System.out.println("Cannot log to file.");
+			ex.printStackTrace();
+		}
+	}
 
-    public static void info(String message) {
-        log( LogLevel.INFO, message);
-    }
+	public static void debug(String message) {
+		log(LogLevel.DEBUG, message);
+	}
 
-    public static void error(String message) {
-        log(LogLevel.ERROR, message);
-    }
+	public static void info(String message) {
+		log( LogLevel.INFO, message);
+	}
+
+	public static void error(String message) {
+		log(LogLevel.ERROR, message);
+	}
 
     public static void log(Exception ex) {
         log(LogLevel.ERROR, ex.getClass().getCanonicalName() + ": " + ex.getMessage());
