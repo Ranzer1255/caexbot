9e5fa47810084b956bc2fb2b70e1408dc4ff6de1
Bobby <sgmaniac1255@yahoo.com>
2016-10-10 23:26:24 -0500

    add notes
     so i dont loose my train of thought

--- src/main/java/ZomDiceControler.java
+++ src/main/java/ZomDiceControler.java
@@ -5,4 +5,14 @@ import net.dv8tion.jda.entities.User;
 public class ZomDiceControler {
 
 	private List<User> plyaers;
+	
+	/*
+	 * TODO Controller for game
+	 * 
+	 * this class will be the unit that takes in commands from Discord
+	 * and passes them to the Game. takes the responces from the game and interperets
+	 * it into ouput via Discord
+	 * 
+	 * (in MVC terms this is the Controler, while the discord Command structure is the View)
+	 */
 }
