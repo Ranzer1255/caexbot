203d01cf5dd9b0214f4006ae3568e8a359e4694b
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-11-02 14:50:10 -0500

    work on music player

---
+++ src/main/java/caexbot/functions/music/GuildPlayerManager.java
@@ -0,0 +1,40 @@
+package caexbot.functions.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import net.dv8tion.jda.entities.Guild;
+
+import java.util.Map;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration.ResamplingQuality;
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+
+public class GuildPlayerManager {
+	
+	private AudioPlayerManager pm;
+
+	private static Map<Guild, AudioPlayer> players;
+	private static GuildPlayerManager instance;
+
+	private GuildPlayerManager() {
+		pm = new DefaultAudioPlayerManager();
+		pm.getConfiguration().setResamplingQuality(ResamplingQuality.LOW);
+		pm.registerSourceManager(new YoutubeAudioSourceManager());
+		pm.registerSourceManager(new SoundCloudAudioSourceManager());
+	}
+
+	public static GuildPlayerManager getInstance() {
+		if (instance == null)
+			instance = new GuildPlayerManager();
+		return instance;
+	}
+	
+	
+}
---
+++ src/main/java/caexbot/functions/music/TrackScheduler.java
@@ -0,0 +1,64 @@
+package caexbot.functions.music;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+public class TrackScheduler extends AudioEventAdapter {
+
+	private Queue<AudioTrack> queue = new LinkedList<>();
+	
+	public void addTrack(AudioTrack track){
+		queue.add(track);
+	}
+
+	public AudioTrack nextTrack(){
+		return queue.remove();
+	}
+
+	@Override
+	public void onPlayerPause(AudioPlayer player) {
+		// Player was paused
+	}
+
+	@Override
+	public void onPlayerResume(AudioPlayer player) {
+		// Player was resumed
+	}
+
+	@Override
+	public void onTrackStart(AudioPlayer player, AudioTrack track) {
+		// A track started playing
+	}
+
+	@Override
+	public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+		if (endReason==AudioTrackEndReason.FINISHED){
+			if(queue.isEmpty())
+				return;
+			player.playTrack(queue.remove());
+			return;
+		}
+		
+		// endReason == FINISHED: A track finished (or died by an exception) - just start the next one
+		// endReason == STOPPED: The player was stopped - makes no sense to start the next track
+		// endReason == REPLACED: Another track started playing while this had not finished, do nothing
+		// endReason == CLEANUP: Player hasn't been queried for a while, if you want you can put a
+		//                       clone of this back to your queue
+	}
+
+	@Override
+	public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException exception) {
+		// An already playing track threw an exception (track end event will still be received separately)
+	}
+
+	@Override
+	public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs) {
+		// Audio track has been unable to provide us any audio, might want to just start a new track
+	}
+}
