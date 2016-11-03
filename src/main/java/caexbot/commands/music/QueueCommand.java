5de148a142521ef5435c034f0d8d5789c6bbcc4d
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-11-03 16:17:24 -0500

    integrate YT search function

--- src/main/java/caexbot/commands/music/QueueCommand.java
+++ src/main/java/caexbot/commands/music/QueueCommand.java
@@ -1,34 +1,51 @@
 package caexbot.commands.music;
 
+import java.util.Arrays;
 import java.util.List;
 
 import caexbot.commands.CaexSubCommand;
+import caexbot.commands.search.YouTubeSearcher;
 import net.dv8tion.jda.entities.TextChannel;
 import net.dv8tion.jda.entities.User;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 public class QueueCommand extends CaexSubCommand {
 
+	private static final String NO_VIDEO_FOUND = "i'm sorry I didn't find that"; 
+
 	@Override
 	public void process(String[] args, User author, TextChannel channel, MessageReceivedEvent event) {
 
 		/*TODO Queue command process
-		 *search for video id
 		 *add id to track queue 
 		 */
-
+		if (args.length<1) {
+			invalidUsage();
+			return;
+		}
+		
+		StringBuilder queryBuilder = new StringBuilder();
+		for (int i = 0; i < args.length; i++) {
+			queryBuilder.append(args[i]);
+		} 
+		YouTubeSearcher yts = new YouTubeSearcher();
+		String videoID = yts.searchForVideo(queryBuilder.toString());
+		
+		if (videoID!=null) {
+			//TODO add to queue
+		} else {
+			channel.sendMessage(NO_VIDEO_FOUND);
+		}
 	}
 
 	@Override
 	public List<String> getAlias() {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.asList("add", "queue");
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Add song to the play queue";
 	}
 
 }
