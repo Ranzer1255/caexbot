fc615594d8615ea7a6bf2f4b125907fd6fe62810
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-11-03 12:38:00 -0500

    refactor YouTube search
    created a Searcher class for youtube searching to allow for code reuse

--- src/main/java/caexbot/commands/search/YoutubeSearchCommand.java
+++ src/main/java/caexbot/commands/search/YoutubeSearchCommand.java
@@ -1,19 +1,9 @@
 package caexbot.commands.search;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
-
 import caexbot.commands.CaexCommand;
-import caexbot.config.CaexConfiguration;
 import caexbot.util.Logging;
 import net.dv8tion.jda.entities.TextChannel;
 import net.dv8tion.jda.entities.User;
@@ -22,49 +12,32 @@ import net.dv8tion.jda.events.message.MessageReceivedEvent;
 public class YoutubeSearchCommand extends CaexCommand {
 
 	private static final String YOUTUBE_BASE_STRING = "https://youtu.be/";
-	private CaexConfiguration config;
+	private YouTubeSearcher yts;
 	
 	public YoutubeSearchCommand() {
-		config = CaexConfiguration.getInstance();
+		yts = new YouTubeSearcher();
 	}
 	@Override
 	public void process(String[] args, User author, TextChannel channel, MessageReceivedEvent event) {
 		StringBuilder queryBuilder = new StringBuilder();
+		
 		for (int i = 0; i < args.length; i++) {
 			queryBuilder.append(args[i]).append(" ");
 		}
-		String query = queryBuilder.toString();
-		Logging.debug(query);
-
-		YouTube yt = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
-			
-			@Override
-			public void initialize(HttpRequest request) throws IOException {}//no-op
-		}).setApplicationName("CaexBot").build();
+		String videoID = yts.searchForVideo(queryBuilder.toString());
 		
-		try {
-			YouTube.Search.List search = yt.search().list("snippet");
-			search.setKey(config.getGToken());
-			search.setQ(query);
-			search.setType("video");
-			
-			SearchListResponse response = search.execute();
-			List<SearchResult> resultList = response.getItems();
-			for (SearchResult searchResult : resultList) {
-				Logging.debug(searchResult.getId().getKind()+" "+searchResult.getSnippet().getTitle());
-			}
-			if (resultList.size()>0){
-				StringBuilder youtubeURL = new StringBuilder().append(YOUTUBE_BASE_STRING)
-						.append(resultList.get(0).getId().getVideoId());
-				channel.sendMessage(author.getAsMention() + " "+ youtubeURL.toString());
-			} else {
-				channel.sendMessage("I'm sorry, i didnt find anything");
-			}
-			
-		} catch (IOException e) {
-			Logging.error(e.getMessage());
-			Logging.log(e);
+		Logging.debug(queryBuilder.toString()+" : " +videoID);
+		
+		if(videoID!=null){
+			StringBuilder youtubeURL = new StringBuilder();
+			youtubeURL.append(YOUTUBE_BASE_STRING).append(videoID);
+			channel.sendMessage(author.getAsMention() + " "+ youtubeURL.toString());
+		} else {
+			channel.sendMessage("I'm sorry, i didn't find anything");			
 		}
+
+		
+
 	}
 
 	@Override
