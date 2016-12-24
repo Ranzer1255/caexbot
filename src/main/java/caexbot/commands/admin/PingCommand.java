942e53de162eb6f1de4462787846ce240035339e
Bobby <sgmaniac1255@yahoo.com>
2016-12-24 17:46:12 -0600

    closes #65

--- src/main/java/caexbot/commands/admin/PingCommand.java
+++ src/main/java/caexbot/commands/admin/PingCommand.java
@@ -1,21 +1,41 @@
 package caexbot.commands.admin;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import caexbot.commands.CaexCommand;
 import caexbot.commands.DraconicCommand;
 import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class PingCommand extends CaexCommand implements DraconicCommand{
 
 	@Override
 	public void process(String[] args, User author, TextChannel channel, MessageReceivedEvent event) {
+		Date startTime = new Date();
+		try {
+			Message pong = channel.sendMessage("pong!").block();
+			Date endTime = new Date();
+			long lag = endTime.getTime()-startTime.getTime();
+			pong.editMessage(pong.getContent()+" `"+lag+"ms`").queue();
+		} catch (RateLimitedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		channel.sendMessage("pong!").queue();	
+//		Date startTime2 = new Date();
+//		channel.sendMessage("pong again! ").queue(pong -> {
+//			Date endTime = new Date();
+//			long lag = endTime.getTime()-startTime2.getTime();
+//			pong.editMessage(pong.getContent()+"`"+lag+"ms`").queue();
+//		});
+		
+		
 	}
 
 	@Override
