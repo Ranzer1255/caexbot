f53c604c5c558eb84d8c882f0746ac41069988de
sgmaniac1255 <sgmaniac1255@yahoo.com>
2017-01-06 11:09:51 -0600

    format all command list into groups of embeds
    i'm not happy with this. will change later

--- src/main/java/caexbot/commands/admin/HelpCommand.java
+++ src/main/java/caexbot/commands/admin/HelpCommand.java
@@ -2,7 +2,9 @@ package caexbot.commands.admin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import caexbot.commands.CaexCommand;
@@ -31,38 +33,51 @@ public class HelpCommand extends CaexCommand implements DraconicCommand, Describ
 	public void process(String[] args, User author, TextChannel channel, MessageReceivedEvent event) {
 		Logging.debug("Help called");
 
-		StringBuilder sb = new StringBuilder();
-		EmbedBuilder eb = new EmbedBuilder();
+//		StringBuilder sb = new StringBuilder();
+		MessageBuilder mb = new MessageBuilder();
 		
+		//single command help line
 		if(args.length==1){
+			EmbedBuilder eb = new EmbedBuilder();
 			Logging.debug("help with arg (" +args[0]+")");
 			Optional<Describable> opt = getDescribables().stream().filter(cc -> cc.getAlias().contains(args[0])).findFirst();
 			Logging.debug(String.valueOf(opt.isPresent()));
 			if(opt.isPresent()){
 				Describable d = opt.get();
-				eb.setAuthor(d.getAlias().get(0), null, null);
+				eb.setAuthor(d.getName(), null, null);
 				eb.setDescription(d.getLongDescription());
 				eb.setColor(d.getCatagory().COLOR);
 				eb.addField("Usage",d.getUsage(event.getGuild()),false);
 				eb.addField("Other Aliases", StringUtil.arrayToString(d.getAlias().subList(1, d.getAlias().size()), ", "), true );
 				eb.addField("Catagory", d.getCatagory().toString(), true);
-				MessageBuilder mb = new MessageBuilder();
 				
 				channel.sendMessage(mb.setEmbed(eb.build()).build()).queue();
-//				
-//				sb.append("**Alias:** ")	 .append("[").append(StringUtil.cmdArrayToString(cmd.getAlias(), ", ",event.getGuild())).append("]\n")
-//				  .append("**Description** ").append(cmd.getDescription()).append("\n")
-//				  .append("**Usage:** ")	 .append(cmd.getUsage(event.getGuild()));
+
 			}
+			
+		//full command list	
 		}else{
+			
+			Map<Catagory, List<Describable>> sorted = new HashMap<>();
+			
 			for (Describable d : getDescribables()) {//TODO catagrize commands via Catagory
-					sb.append(String.format(
-							"**[%s]**: %s\n", 
-							StringUtil.cmdArrayToString(d.getAlias(), ", ", event.getGuild()),
-							d.getShortDescription()));
-				
+				if(sorted.get(d.getCatagory())==null){
+					sorted.put(d.getCatagory(), new ArrayList<>());
+				}
+				sorted.get(d.getCatagory()).add(d);				
 			}	
-			channel.sendMessage(sb.toString()).queue();//TODO change else block into Embeds
+			
+			for (Catagory cat : sorted.keySet()) {
+				EmbedBuilder eb = new EmbedBuilder();
+				eb.setAuthor(cat.NAME, null, null);
+				eb.setColor(cat.COLOR);
+				for (Describable d : sorted.get(cat)) {
+					eb.addField(d.getName(), d.getShortDescription(), false);
+				}
+				mb.setEmbed(eb.build());
+				channel.sendMessage(mb.build()).queue();
+			}
+			
 		}
 	}
 
