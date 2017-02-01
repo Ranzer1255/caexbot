1a44fff1625230d0f0671bfbf86b2ca9c3d861a0
Bobby <sgmaniac1255@yahoo.com>
2017-01-31 21:24:25 -0600

    extract the MusicListener class from GuildData
    it had no buisness being an inner class of GuildData

--- src/main/java/caexbot/data/GuildData.java
+++ src/main/java/caexbot/data/GuildData.java
@@ -5,17 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import caexbot.commands.music.MusicCommand;
 import caexbot.config.CaexConfiguration;
 import caexbot.database.CaexDB;
 import caexbot.functions.levels.UserLevel;
-import caexbot.functions.music.MusicEventListener;
-import caexbot.functions.music.events.MusicEvent;
-import caexbot.functions.music.events.MusicJoinEvent;
-import caexbot.functions.music.events.MusicLoadEvent;
-import caexbot.functions.music.events.MusicSkipEvent;
-import caexbot.functions.music.events.MusicStartEvent;
-import caexbot.functions.music.events.PlaylistLoadEvent;
+import caexbot.functions.music.MusicListener;
 import caexbot.util.Logging;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.TextChannel;
@@ -137,55 +130,6 @@ public class GuildData {
 	 */
 	public void setMusicChannel(TextChannel musicChannel) {
 		this.musicListener.setMusicChannel(musicChannel);
-	}
-
-	public class MusicListener implements MusicEventListener{
-		private TextChannel musicChannel;
-	
-		public MusicListener() {
-		}
-	
-		public TextChannel getMusicChannel() {
-			return musicChannel;
-		}
-	
-		public void setMusicChannel(TextChannel musicChannel) {
-			this.musicChannel = musicChannel;
-		}
-
-		@Override
-		public void handleEvent(MusicEvent event) {
-			
-			
-			if(event instanceof MusicJoinEvent){
-				getMusicChannel().sendMessage(String.format(MusicCommand.JOIN, ((MusicJoinEvent) event).getChannelJoined().getName())).queue();
-			}
-			
-			else if (event instanceof MusicStartEvent){
-				getMusicChannel().sendMessage(String.format(MusicCommand.NOW_PLAYING, "youtu.be/"+((MusicStartEvent) event).getSong().getIdentifier())).queue();
-			}
-			
-			else if (event instanceof MusicSkipEvent){
-				getMusicChannel().sendMessage("Skipping the rest of the Current song :stuck_out_tongue:").queue();
-			}
-			
-			else if (event instanceof MusicLoadEvent){
-				getMusicChannel().sendMessage(String.format("Loaded %s sucssesfuly\n%s", 
-						((MusicLoadEvent) event).getSong().getInfo().title, "youtu.be/"+((MusicLoadEvent) event).getSong().getIdentifier())).queue();
-			}
-			
-			else if (event instanceof PlaylistLoadEvent){
-				getMusicChannel().sendMessage(String.format("Loaded Playlist: %s",
-						((PlaylistLoadEvent) event).getList().getName())).queue();
-			}
-			
-			else{
-				getMusicChannel().sendMessage("This music event isn't hanndled yet.... Yell at ranzer ("+event.getClass().getSimpleName()+")").queue();
-			
-			}
-			
-			
-		}
 	}
 	
 	
