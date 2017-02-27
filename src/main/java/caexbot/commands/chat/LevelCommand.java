206f92b9d5ba96a0c48ec0b68e946ca9094ef25b
sgmaniac1255 <sgmaniac1255@yahoo.com>
2017-02-27 14:02:13 -0600

    rebuilt to manage guild levels

--- src/main/java/caexbot/commands/chat/LevelCommand.java
+++ src/main/java/caexbot/commands/chat/LevelCommand.java
@@ -2,7 +2,6 @@ package caexbot.commands.chat;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import caexbot.commands.CaexCommand;
 import caexbot.commands.Catagory;
@@ -85,17 +84,17 @@ public class LevelCommand extends CaexCommand implements DraconicCommand,Describ
 	private String rankMessage(String[] args, User author, TextChannel channel, MessageReceivedEvent event) {
 		StringBuilder msg = new StringBuilder();
 		
-		List<Map.Entry<User, UserLevel>> rankings = GuildManager.getGuildData(event.getGuild()).getGuildRankings();
+		List<UserLevel> rankings = GuildManager.getGuildData(event.getGuild()).getGuildRankings();
 		
 		msg.append("__***Current Leaderboard***__\nall XP is beta and will be reset\n\n");
 		int index=0;
-		for (Map.Entry<User, UserLevel> entry : rankings) {
+		for (UserLevel entry : rankings) {
 			if(index++>=10) break;
 			msg.append(
 				String.format("__**%s**__:\t*Level:* **%s** with __%sxp*__\n", 
-					channel.getGuild().getMember(entry.getKey()).getEffectiveName(), 
-					entry.getValue().getLevel(),
-					entry.getValue().getXP()
+					entry.getMember().getEffectiveName(), 
+					entry.getLevel(),
+					entry.getXP()
 				)
 			);
 		}
