6faf52da750d7af8d684e3c8e8318911612c9cb3
Bobby <sgmaniac1255@yahoo.com>
2017-03-12 14:08:24 -0500

    fill in Describable

--- src/main/java/caexbot/data/ChannelData.java
+++ src/main/java/caexbot/data/ChannelData.java
@@ -2,7 +2,7 @@ package caexbot.data;
 
 public interface ChannelData {
 
-	public final boolean DEFAULT_SETTING = true;
+	public final boolean DEFAULT_XP_SETTING = true;
 	
 	public void setXPPerm(boolean earnEXP);
 	
