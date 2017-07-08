a87c9a365e714b9f1c100f912a59457523cace11
Bobby <sgmaniac1255@yahoo.com>
2017-07-07 23:01:38 -0500

    cleaned up how DieRoll works.
    consolodated the randomizer into a single method and added a less
    verbose results method that doesn't show each die in the results.

--- src/main/java/caexbot/functions/dice/DieRoll.java
+++ src/main/java/caexbot/functions/dice/DieRoll.java
@@ -4,49 +4,86 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class DieRoll {
 
-	int numDice, dieSize, modifier;
+	private final int numDice, dieSize, modifier;
+	private boolean isRolled;
+	private int[] results;
+	private int total;
 	
+	public int getNumDice() {
+		return numDice;
+	}
+
+	public int getDieSize() {
+		return dieSize;
+	}
+
+	public int getModifier() {
+		return modifier;
+	}
+
+	public boolean isRolled() {
+		return isRolled;
+	}
+
+	public int[] getResults() {
+		return results;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
 	public DieRoll(int number, int size, int modifier) {
 		numDice = number;
 		dieSize = size;
 		this.modifier = modifier;
+		isRolled=false;
+		results = new int[number];
 	}
 	
 	public int roll(){
 		
-		int result=0;
+		total=0;
 		for(int i = 0;i<numDice;i++){
-			result += ThreadLocalRandom.current().nextInt(0, dieSize)+1;
+			int roll = ThreadLocalRandom.current().nextInt(0, dieSize)+1;
+			results[i]=roll;
+			total += roll;
+			
 		}
-		result += modifier;
-		
-		return result;
+		total += modifier;
+		isRolled = true;
+		return total;
 	}
 	
-	public String Result(){
+	public String result(){
+	
+		return String.format("Rolling %dd%d = %d", numDice, dieSize, total);
+	}
+	
+	public String verboseResult(){
+		if(!isRolled){
+			roll();
+		}
 		StringBuilder rtn = new StringBuilder();
 		
 		rtn.append("Rolling ").append(numDice).append("d").append(dieSize);
 		if(modifier<0)
 			rtn.append(modifier);
+		else if (modifier == 0);
 		else 
 			rtn.append("+").append(modifier);
 		
 		rtn.append(" = (");
-		int total=0;
-		for(int i = 0;i<numDice;i++){
-			
-			int result = ThreadLocalRandom.current().nextInt(0, dieSize)+1;
-			rtn.append(result).append(", ");
-			total+=result;
+		
+		for(int i = 0;i<results.length;i++){
+			rtn.append(results[i]).append(", ");
 		}
 		rtn.delete(rtn.length()-2, rtn.length()).append(")");
 		if(modifier<0)
 			rtn.append(modifier);
+		else if (modifier==0);
 		else 
 			rtn.append("+").append(modifier);
-		
-		total += modifier;
 		rtn.append(" = ").append(total);
 		
 		return rtn.toString();
